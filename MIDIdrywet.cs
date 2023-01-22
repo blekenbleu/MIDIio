e6d09e46188428e987f4dcdd8ff26062178d9c6d
blekenbleu <blekenbleu@gmail.com>
2023-01-22 10:53:37 -0500

    Rename MIDIioSettings class to MIDIdrywet

--- MIDIdrywet.cs
+++ MIDIdrywet.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Settings class, make sure it can be correctly serialized using JSON.net
     /// </summary>
-    public class MIDIioSettings
+    public class MIDIdrywet
     {
         public int SpeedWarningLevel = 100;
     }
