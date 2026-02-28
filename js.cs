e86c390e484f465fd386f155478870ffa295dd7c
Steven Carmichael <blekenbleu@gmail.com>
2026-02-28 14:30:53 -0500

    bug fix: non-integer simValues[slider].Default

--- js.cs
+++ js.cs
@@ -54,7 +54,7 @@ namespace blekenbleu
 			 ; 100 (1)		0 - 100
 			 ; 1000 (10)	0 - 1000
 			 */
-			if (100 < Convert.ToInt32(simValues[slider].Default))
+			if (100 < Convert.ToDouble(simValues[slider].Default))
 			{
 				SliderFactor[0] = 10;	// slider to value
 				SliderFactor[1] = 0.1;	// value to slider
